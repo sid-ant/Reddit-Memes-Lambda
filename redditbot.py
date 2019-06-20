793eb089fb1fcb9bdce6f51f43d069ffd68ac98b
Siddhant Chhabra <siddhant1042@gmail.com>
2019-06-21 02:19:31 +0530

    Async call to telegram API

--- redditbot.py
+++ redditbot.py
@@ -1,8 +1,8 @@
 import praw
 import os
 import logging
-import requests 
 import boto3 
+from requests_futures.sessions import FuturesSession
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,7 +14,7 @@ def lambda_handler(event,context):
     try:
         dank = get_dem_memes()
         subsribers = get_chats()
-        #boys_go_deliver(dank,subsribers)
+        boys_go_deliver(dank,subsribers)
     except:
         logger.info("opps, something bad happened")
         raise
@@ -29,10 +29,10 @@ def get_dem_memes():
 
     logger.info(f"The reddit is in {reddit.read_only}")
     memes=[]
-    for submission in reddit.subreddit("memes").top('hour',limit=10):
+    for submission in reddit.subreddit("memes").top('day',limit=10):
         if submission.url.find("i.redd.it")!=-1:
             memes.append(submission.url)
-    memes = memes[:5]
+    memes = memes[:6]
     logger.info(f"Got memes from reddit {memes}")
     return memes    
 
@@ -57,11 +57,12 @@ def get_chats():
 def boys_go_deliver(good_stuff,audience):
     accesscode = os.environ['accesscode']
     try:
+        session = FuturesSession()
         url = f"https://api.telegram.org/bot{accesscode}/sendPhoto"
         logger.info(f"url formed is {url}")
-        for stuff in good_stuff:
-            response = requests.post(url, data={'chat_id':chat_id,'photo':stuff})
+        for chat_id in audience:
+            for stuff in good_stuff:
+                response = session.post(url, data={'chat_id':chat_id,'photo':stuff})
         logger.info(f"Successfully sent message! {good_stuff}")
-    except RequestException as e:
-        logger.error(f"Couldn't send reply {e}")
-    return None
+    except:
+        logger.error(f"Couldn't send reply")
